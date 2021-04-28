a90f62691a12559acc221f30997e1d5f256ae9b1
kano89125 <33770868+kano89125@users.noreply.github.com>
2021-04-29 00:33:05 +0800

    Add files via upload
    get_state_properties_a : Si -> Si+2 的盤面計算  須給參數 is_black 來源是judge程式給的
    目前版本中所有給出的 properties 皆為 [ 黑分數 白分數 黑線數 白線數]的 list
    
    random_gen_board(steps) : 產生隨機合法的盤面以及移動方式，就是每次random 一個合法移動而已。

---
+++ util.py
@@ -0,0 +1,181 @@
+import numpy as np
+
+
+def get_state_properties_a(pre_state, pre_state_properties, now_state, is_black):
+    temp_state = now_state - pre_state
+    movements = np.zeros([2, 4])
+    for l in range(6):
+        for i in range(6):
+            for j in range(6):
+                if temp_state[l][i][j] == 1:
+                    movements[1 if is_black else 0, :] = (l, i, j, 1)
+                elif temp_state[l][i][j] == 2:
+                    movements[0 if is_black else 1, :] = (l, i, j, 2)
+    ret = get_state_properties(pre_state, pre_state_properties, movements)
+    return ret
+
+
+def boundary_test(coordinate):
+    for i in range(3):
+        if coordinate[i] < 0 or coordinate[i] >= 6:
+            return False
+    return True
+
+
+def get_state_properties_b(start_state, start_state_properties, movements):
+    dirs = []
+    vec = [0, 1]
+    for l in vec:
+        for i in vec:
+            for j in vec:
+                if l == 0 and i == 0 and j == 0:
+                    continue
+                dirs.append([l, i, j])
+
+    temp_state = start_state
+    temp_properties = start_state_properties
+    for move in movements:
+        possible_dir = []
+        l, i, j, c = move[0], move[1], move[2], move[3]
+        for dir in dirs:
+            muls = np.arange(1, 4)
+            cnt = 1
+            for mul in muls:
+                temp_coordinate = np.add(move[0:3], np.multiply(dir, mul))
+                if not boundary_test(temp_coordinate):
+                    break
+                temp_l, temp_i, temp_j = \
+                    temp_coordinate[0], temp_coordinate[1], temp_coordinate[2]
+                if temp_state[temp_l, temp_i, temp_j] != c:
+                    break
+                cnt += 1
+            for mul in muls:
+                temp_coordinate = np.add(move[0:3], np.multiply(dir, -mul))
+                if not boundary_test(temp_coordinate):
+                    break
+                temp_l, temp_i, temp_j = \
+                    temp_coordinate[0], temp_coordinate[1], temp_coordinate[2]
+                if temp_state[temp_l, temp_i, temp_j] != c:
+                    break
+                cnt += 1
+            while cnt >= 4:
+                print(cnt)
+                temp_properties[c + 1] += 1
+                temp_properties[c - 1] +=  \
+                    100 / (temp_properties[2] + temp_properties[3])
+                cnt -= 1
+        temp_state[l, i, j] = c
+    return temp_properties
+
+
+def get_state_properties_b_plum(start_state, start_state_properties, movements):
+    dirs = []
+    vec = [0, 1]
+    for l in vec:
+        for i in vec:
+            for j in vec:
+                if l == 0 and i == 0 and j == 0:
+                    continue
+                dirs.append([l, i, j])
+
+    temp_state = start_state
+    temp_properties = start_state_properties
+    for move in movements:
+        possible_dir = []
+        l, i, j, c = move[0], move[1], move[2], move[3]
+        for dir in dirs:
+            muls = np.arange(1, 4)
+            cnt = 1
+            for mul in muls:
+                temp_coordinate = np.add(move[0:3], np.multiply(dir, mul))
+                if not boundary_test(temp_coordinate):
+                    break
+                temp_l, temp_i, temp_j = \
+                    temp_coordinate[0], temp_coordinate[1], temp_coordinate[2]
+                if temp_state[temp_l, temp_i, temp_j] != c:
+                    break
+                cnt += 1
+            for mul in muls:
+                temp_coordinate = np.add(move[0:3], np.multiply(dir, -mul))
+                if not boundary_test(temp_coordinate):
+                    break
+                temp_l, temp_i, temp_j = \
+                    temp_coordinate[0], temp_coordinate[1], temp_coordinate[2]
+                if temp_state[temp_l, temp_i, temp_j] != c:
+                    break
+                cnt += 1
+            while cnt >= 4:
+                print(cnt)
+                temp_properties[c + 1] += 1
+                temp_properties[c - 1] +=  \
+                    100 / (temp_properties[2] + temp_properties[3])
+                cnt -= 1
+        print(move)
+        print(temp_properties)
+        input()
+        temp_state[l, i, j] = c
+    return temp_properties
+
+
+def get_next_possible_move(state):
+    possible_moves = []
+    for i in range(6):
+        for j in range(6):
+            if state[5][i][j] != 0:
+                continue
+            for l in range(6):
+                if state[l][i][j] == 0:
+                    possible_moves.append([l, i, j])
+                    break
+    ret = np.array(possible_moves)
+    return ret
+
+
+#pre_board = np.zeros([6,6,6])
+# board = np.zeros([6,6,6])
+# board[0,3,3] = 1
+# board[0,2,3] = 2
+# properties = np.zeros([4])
+#ret = get_state_properties_a(pre_board,properties,board,True)
+#ret = get_next_possible_move(pre_board)
+#boundary_test([2, 4, 1])
+
+# move = [1,2,3,4]
+# l, i, j = move[0], move[1], move[2]
+# bb = np.zeros([5,5,5])
+# bb[l,i, j] = 1
+# print(bb)
+#np.random.randint(3, size = (6, 6, 6))
+
+
+def random_gen_board(steps):
+    board = np.zeros([6, 6, 6])
+    for l in range(6):
+        board[l, 0, 0] = -1
+        board[l, 0, 1] = -1
+        board[l, 0, 4] = -1
+        board[l, 0, 5] = -1
+        board[l, 1, 0] = -1
+        board[l, 1, 5] = -1
+        board[l, 4, 0] = -1
+        board[l, 4, 5] = -1
+        board[l, 5, 0] = -1
+        board[l, 5, 1] = -1
+        board[l, 5, 4] = -1
+        board[l, 5, 5] = -1
+    movements = []
+    for step_num in range(steps):
+        possible_moves = get_next_possible_move(board)
+        choose_index = np.random.randint(len(possible_moves))
+        l, i, j = possible_moves[choose_index][0], possible_moves[choose_index][1], possible_moves[choose_index][2]
+        c = step_num % 2 + 1
+        movements.append([l, i, j, c])
+        board[l, i, j] = c
+    print(board)
+    return movements
+
+
+movements = random_gen_board(60)
+print(get_state_properties_b(np.zeros([6, 6, 6]), [0, 0, 0, 0], movements))
+print(get_state_properties_b_plum(
+    np.zeros([6, 6, 6]), [0, 0, 0, 0], movements))
